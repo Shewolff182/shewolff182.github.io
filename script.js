7f4c2c4f8b5a28df4ea01ee3a54d49dd1ef2d562
shan wolff <shanwolff182@gmail.com>
2024-11-14 14:14:30 -0800

    updated somethings

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.getElementById("submit-btn").addEventListener("click", function() {
             message = "Even when times are tough, keep in mind that brighter days are ahead. Take it one day, minute, second at a time... each moment will bring you closer to healing!";
             break;
         case "anxious":
-            message = "One step at a time. You do not have to have it all figured out right now.Remember to breathe... ground yourself in this moment and know that you will get through this too!";
+            message = "One step at a time. You do not have to have it all figured out right now. Remember to breathe... ground yourself in this moment and know that you will get through this too!";
             break;
         case "excited":
             message = "Embrace your enthusiasm and allow it to fuel your dreams. The universe is giving you a reason to smile! This energy will lead you far on your path!";
